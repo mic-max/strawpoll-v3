ccb3fe37829f294edb313ca2f84844d913d41591
Michael Maxwell <mike.ian.maxwell@gmail.com>
2026-01-14 16:11:30 -0500

    Update index.html

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,15 +1,17 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import CreatePollForm from "./components/CreatePollForm";
 import VotePoll from "./components/VotePoll";
+import PollResults from "./components/PollResults";
 
 function App() {
   return (
     <>
-      <h1>Strawpoll</h1>
+      <a href='/'><h1>Straw Poll</h1></a>
       <Router>
         <Routes>
           <Route path="/" element={<CreatePollForm />} />
-          <Route path="/vote/:pollId" element={<VotePoll />} />
+          <Route path="/:pollId" element={<VotePoll />} />
+          <Route path="/:pollId/results" element={<PollResults />} />
         </Routes>
       </Router>
     </>
